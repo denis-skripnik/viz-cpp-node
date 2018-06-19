a0f4d2aba14932513c7a57c5de8050ddd8c88063
Anatoly Piskunov <anatoly.piskunov@gmail.com>
2018-06-19 13:42:28 +0000

    Remove frozen comments/posts depending on cashout time

--- libraries/chain/steem_evaluator.cpp
+++ libraries/chain/steem_evaluator.cpp
@@ -369,14 +369,6 @@ namespace golos { namespace chain {
                 auto now = _db.head_block_time();
 
                 if (itr == by_permlink_idx.end()) {
-                    if (o.parent_author != STEEMIT_ROOT_POST_PARENT) {
-                        if (_db.has_hardfork(STEEMIT_HARDFORK_0_12__177) && !_db.has_hardfork( STEEMIT_HARDFORK_0_18__536) ) {
-                            FC_ASSERT(
-                                    _db.calculate_discussion_payout_time(*parent) !=
-                                    fc::time_point_sec::maximum(), "Discussion is frozen.");
-                        }
-                    }
-
                     if (_db.has_hardfork(STEEMIT_HARDFORK_0_12__176)) {
                         if (o.parent_author == STEEMIT_ROOT_POST_PARENT)
                             FC_ASSERT((now - auth.last_root_post) >
@@ -480,16 +472,6 @@ namespace golos { namespace chain {
                 } else // start edit case
                 {
                     const auto &comment = *itr;
-                    if ( !_db.has_hardfork( STEEMIT_HARDFORK_0_18__536 ) ) {
-                        if (_db.has_hardfork(STEEMIT_HARDFORK_0_14__306)) {
-                            FC_ASSERT(_db.calculate_discussion_payout_time(comment) != fc::time_point_sec::maximum(),
-                                      "The comment is archived.");
-                        } else if (_db.has_hardfork(STEEMIT_HARDFORK_0_10)) {
-                            FC_ASSERT(comment.last_payout == fc::time_point_sec::min(),
-                                      "Can only edit during the first 24 hours.");
-                        }
-                    }
-
                     _db.modify(comment, [&](comment_object &com) {
                         com.last_update = _db.head_block_time();
                         com.active = com.last_update;
