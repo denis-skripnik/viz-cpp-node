steemit/steem

Fix replay #551

libraries/chain/include/golos/chain/database_exceptions.hpp
@@ -111,6 +111,8 @@ namespace golos {
 
         STEEMIT_DECLARE_INTERNAL_EXCEPTION(verify_auth_account_not_found, 2, "Auth account not found")
 
+        FC_DECLARE_DERIVED_EXCEPTION(database_revision_exception, golos::chain::chain_exception, 4120000, "database revision exception")
+
     }
 } // golos::chain
 

denis-skripnik/viz-cpp-node

libraries/chain/include/graphene/chain/database_exceptions.hpp
@@ -115,6 +115,8 @@ namespace graphene {
 
         FC_DECLARE_DERIVED_EXCEPTION(database_signal_exception, graphene::chain::chain_exception, 4130000, "database signal exception")
 
+        FC_DECLARE_DERIVED_EXCEPTION(database_revision_exception, graphene::chain::chain_exception, 4120000, "database revision exception")
+
     }
 } // graphene::chain
 