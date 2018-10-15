steemit/steem

Added support for POSIX signals in the replay #551

libraries/chain/include/golos/chain/database_exceptions.hpp
@@ -113,6 +113,8 @@ namespace golos {
 
         FC_DECLARE_DERIVED_EXCEPTION(database_revision_exception, golos::chain::chain_exception, 4120000, "database revision exception")
 
+        FC_DECLARE_DERIVED_EXCEPTION(database_signal_exception, golos::chain::chain_exception, 4130000, "database signal exception")
+
     }
 } // golos::chain
 

denis-skripnik/viz-cpp-node

libraries/chain/include/graphene/chain/database_exceptions.hpp
@@ -117,6 +117,8 @@ namespace graphene {
 
         FC_DECLARE_DERIVED_EXCEPTION(database_revision_exception, graphene::chain::chain_exception, 4120000, "database revision exception")
 
+        FC_DECLARE_DERIVED_EXCEPTION(database_signal_exception, graphene::chain::chain_exception, 4130000, "database signal exception")
+
     }
 } // graphene::chain
 